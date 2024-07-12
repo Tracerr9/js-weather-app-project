20607e919147d5ed4e096f72fe3a48c6153ab62a
Tracerr9 <143943431+Tracerr9@users.noreply.github.com>
2024-07-12 20:42:35 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// get weather data
+
+
+
+document.getElementById('submitButton').addEventListener('click', () => {
+  const apiKey = '8e9ee0c614a34c81a95103757241207';
+  const city = document.getElementById('cityName').value;
+  const encodedCity = encodeURIComponent(city)
+
+  const apiUrl =`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodedCity}&aqi=no`;
+  getWeather(apiUrl)
+})
+
+
+function getWeather(apiUrl) {
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Process and display the weather data
+    displayWeather(data)
+  })
+  .catch(error => {
+    console.error('Error fetching weather data:', error);
+  });
+}
+
+function displayWeather(data) {
+  let html = '';
+  html = 
+  `<h2>
+  ${data.location.name} - ${data.location.country}
+  </h2>
+  <img class="condition-img" src='${data.current.condition.icon}'>
+  ${data.current.condition.text} 
+  <div class="temp">
+  <img src="thermometer-icon.png"> ${data.current.temp_c} &deg;C
+  </div>
+  `
+
+  document.getElementById('weather-info').innerHTML = html
+
+}
